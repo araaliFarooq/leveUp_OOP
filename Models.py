de7c0643591a98f4495d0991a7827cddd5ec7c6b
araalifarooq <farooqp35h@gmail.com>
2018-08-09 19:47:59 +0300

    Add user model class [Finishes #159623341]

---
+++ Models.py
@@ -0,0 +1,6 @@
+class Users(object):
+    def __init__(self, first_name, second_name, contact, email):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.contact = contact
+        self.email = email
