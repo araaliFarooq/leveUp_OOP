416b9560781e9e5e875c8cc72b955bbf6ff462a6
araalifarooq <farooqp35h@gmail.com>
2018-08-10 16:44:21 +0300

    Edit views class to finish the submit function

--- Views.py
+++ Views.py
@@ -2,28 +2,52 @@ from Models import Users
 from validation import validate_contact, validate_email
 
 
-users =[]
+all_users =[]
 
 def combine_names(first_name, second_name):
     full_name = first_name +" "+ second_name
     return full_name
 
+# def show_all_users():
+#     if len(users) > 0:
+#         print( user.__dict__ for user in users)
+
+    # print("No users added yet")    
+
 def submit():
     first_name = input("Enter your first name: ")
     second_name = input("Enter your last name: ")
-    contact = input("Enter your phone number: ")
-    
-    if not validate_contact(contact):
-        print("Wrong phone number entered")
-
-    email = input("Enter your email address: ")
-
-    if not validate_email(email):
-        print("Wrong email address entered")
+     
+    while True:
+        contact = input("Enter your phone number [format : xxx-xxxxxxx]: ")
+        if validate_contact(contact) == False:
+            print("Wrong phone number entered")
+            continue
+        else:
+            break   
+        
+    while True:
+        email = input("Enter your email address [format : xxxxx@x.x] : ")
+        if validate_email(email) == False:
+            print("Wrong email address entered")
+            continue
+        else:
+            break    
 
     full_name = combine_names(first_name, second_name)
 
-    new_user = Users(first_name, second_name, contact, email)       
+    new_user = Users(full_name, contact, email)
+    all_users.append(new_user)
+
+    print("New user added:")
+
+    for user in all_users:
+        print( "full name: "+user.full_name )
+        print( "Contact: "+user.contact )
+        print( "Email: "+user.email )
+
+if __name__ == '__main__':
+    submit()    
         
      
 
--- validation.py
+++ validation.py
@@ -2,15 +2,17 @@ import re
 
 def validate_email(email):
     if len(email) > 7:
-        if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+        if re.match("[^@]+@[^@]+\.[^@]+", email) != None:
             return True
         return False
-    # return "Very short email entered"
+    return False    
+    
 
 def validate_contact(contact):
     if len(contact) > 6:
-        if re.match(r'[789]\d{9}$',contact):
+        if re.match("^\d{3}-\d{7}$",contact):
             return True
 
         return False
-    # return "Phone number has to be longer than 6 characters"    
+    return False
+       
