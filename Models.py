c63a0eab6e34eed7d27f11eebe73b49fea0bae1c
araalifarooq <farooqp35h@gmail.com>
2018-08-10 14:32:17 +0300

    Edit model class to change user attributes

--- Models.py
+++ Models.py
@@ -1,6 +1,5 @@
 class Users(object):
-    def __init__(self, first_name, second_name, contact, email):
-        self.first_name = first_name
-        self.second_name = second_name
+    def __init__(self, full_name, contact, email):
+        self.full_name = full_name
         self.contact = contact
         self.email = email
